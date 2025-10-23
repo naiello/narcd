8d576519e4f6b2c124385394e4437f1f043b3fdb
Nick Aiello <naiello606@gmail.com>
2025-10-22 22:01:48 -0500

    feat: initialize default directories on deployment

---
+++ codedeploy/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euxo pipefail
+id -u narcd >/dev/null || useradd --system --shell /sbin/nologin --no-create-home narcd
+for d in /var/log/narcd /var/db/narcd; do
+    mkdir -p "$d"
+    chown -R narcd:narcd "$d"
+done
