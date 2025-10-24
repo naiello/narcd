cf70880d9cc62550b6a661dd18f086c513eb8453
Nick Aiello <naiello606@gmail.com>
2025-10-23 22:10:18 -0500

    chore: enable info logging

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use anyhow::Result;
 use narcd::config::Config;
 use narcd::listeners::ssh::start_server;
 use narcd::logger::FileLogger;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
     pretty_env_logger::init_timed();
+
     let config: Config = Default::default();
     let logger = FileLogger::new(&config.log.dir).await?;
     start_server(&config.listeners.ssh, logger).await?;
+
     Ok(())
 }
